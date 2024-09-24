5e0dad0aa4d478fa7fb2fe8b96486b9b813aaf84
neeraj kumar <neerajkumar08697@gmail.com>
2024-09-24 11:28:43 +0530

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/shopping-view/Home";
+import NotFound from "./pages/not-found/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
