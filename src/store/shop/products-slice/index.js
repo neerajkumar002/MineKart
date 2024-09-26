3cc86951acd72865a25b49e15bf5ea9f87b054ce
neeraj kumar <neerajkumar08697@gmail.com>
2024-09-26 21:49:53 +0530

    feat: fetch product data

--- src/store/shop/products-slice/index.js
+++ src/store/shop/products-slice/index.js
@@ -1,16 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isLoading: false,
   productsList: [],
 };
 
+export const fetchProductsDetails = createAsyncThunk(
+  "/shop/products",
+  async () => {
+    const result = await axios.get("https://fakestoreapi.com/products");
+    return result?.data;
+  }
+);
+
 const shoppingProductSlice = createSlice({
   name: "shoppingProducts",
   initialState,
   reducers: {
-    setProducts: () => {},
+    setProducts: (state) => {
+      state.productsList = null;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProductsDetails.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProductsDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productsList = action.payload;
+      })
+      .addCase(fetchProductsDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productsList = [];
+      });
   },
 });
+
+export const { setProducts } = shoppingProductSlice.actions;
 
 export default shoppingProductSlice.reducer;
