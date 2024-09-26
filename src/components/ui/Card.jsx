fb606612dce62ace2a5f85fa00d86fdfc0384a66
neeraj kumar <neerajkumar08697@gmail.com>
2024-09-26 21:49:05 +0530

    add product card

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,16 +2,16 @@ import Button from "./Button";
 import "../../styles/card.css";
 import "../../styles/utilis.css";
 
-const Card = ({ img, title,  price }) => {
+const Card = ({ title, img, price, description }) => {
   return (
     <div className="product-card flex flex-col items-center gap-1">
       <div className="product-image">
         <img src={img} alt={title} />
       </div>
-      <p>{title}</p> 
+      <p>{title}</p>
       <p className="product-price">${price}</p>
       <div className="btn-container">
-        <Button label="Add To Cart"  />
+        <Button label="Add To Cart" />
       </div>
     </div>
   );
