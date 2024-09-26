d3296215a35ce27ed0d6ddc4fb48e29ad6041e37
neeraj kumar <neerajkumar08697@gmail.com>
2024-09-26 21:49:35 +0530

    render all products

--- src/pages/shopping-view/Home.jsx
+++ src/pages/shopping-view/Home.jsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import Card from "../../components/ui/Card";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductsDetails } from "../../store/shop/products-slice";
+import "../../styles/home.css";
 const Home = () => {
+  const dispatch = useDispatch();
+  const { productsList, isLoading } = useSelector(
+    (state) => state.shopProducts
+  );
+  console.log(productsList);
+  useEffect(() => {
+    dispatch(fetchProductsDetails());
+  }, [dispatch]);
+
+  if (isLoading) return <div>Loading..</div>;
   return (
-    <div>
-      Shopping view home
+    <div className="grid products-container">
+      {productsList.map(({ id, image, title, description, price }) => (
+        <Card
+          key={id}
+          img={image}
+          title={title}
+          description={description}
+          price={price}
+        />
+      ))}
+      {/* 
       <Card
         img={
           "https://chriscross.in/cdn/shop/files/ChrisCrossPlainNavyBlueT-Shirt_1024x.jpg?v=1695720154"
@@ -11,7 +33,7 @@ const Home = () => {
         title="Blue T-shirt"
         description="this is a blue tshirt it is best and humen clothing"
         price="200"
-      />
+      /> */}
     </div>
   );
 };
