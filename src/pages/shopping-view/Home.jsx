a8c563129e9e083924deb4559d2c0a122fb78f71
neeraj kumar <neerajkumar08697@gmail.com>
2024-10-05 16:08:45 +0530

    add categories

--- src/pages/shopping-view/Home.jsx
+++ src/pages/shopping-view/Home.jsx
@@ -2,7 +2,16 @@ import { useEffect } from "react";
 import Card from "../../components/ui/Card";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductsDetails } from "../../store/shop/products-slice";
-import "../../styles/home.css";
+import "../../styles/shopping-view/home.css";
+import { WashingMachine, Gem, Shirt, Flower } from "lucide-react";
+
+const categoriesWithIcon = [
+  { id: "electronics", label: "Electronics", icon: WashingMachine },
+  { id: "jewelery", label: "Jewelery", icon: Gem },
+  { id: "men's clothing", label: "Men's Clothing", icon: Shirt },
+  { id: "women's clothing", label: "Women's Clothing", icon: Flower },
+];
+
 const Home = () => {
   const dispatch = useDispatch();
   const { productsList, isLoading } = useSelector(
@@ -14,26 +23,34 @@ const Home = () => {
   }, [dispatch]);
 
   if (isLoading) return <div>Loading..</div>;
+
   return (
-    <div className="grid products-container">
-      {productsList.map(({ id, image, title, description, price }) => (
-        <Card
-          key={id}
-          img={image}
-          title={title}
-          description={description}
-          price={price}
-        />
-      ))}
-      {/* 
-      <Card
-        img={
-          "https://chriscross.in/cdn/shop/files/ChrisCrossPlainNavyBlueT-Shirt_1024x.jpg?v=1695720154"
-        }
-        title="Blue T-shirt"
-        description="this is a blue tshirt it is best and humen clothing"
-        price="200"
-      /> */}
+    <div>
+      <section className="category-section">
+        <div className="category-heading">
+          <h1>Shop By Category</h1>
+        </div>
+        <div className="category-content">
+          {categoriesWithIcon.map((categoryItem) => (
+            <div className="category-item" key={categoryItem.id}>
+              <categoryItem.icon size={40} className="category-icon" />
+              <span>{categoryItem.label}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* <div className="grid products-container">
+        {productsList.map(({ id, image, title, description, price }) => (
+          <Card
+            key={id}
+            img={image}
+            title={title}
+            description={description}
+            price={price}
+          />
+        ))}
+      </div> */}
     </div>
   );
 };
