a4f0a41bb1d6f371adb37e3aec1c0dfcd9aae869
neeraj kumar <neerajkumar08697@gmail.com>
2024-10-11 11:09:49 +0530

    add nav Items

--- src/components/shopping-view/Header.jsx
+++ src/components/shopping-view/Header.jsx
@@ -1,18 +1,57 @@
-import {   ShoppingCartIcon, User } from "lucide-react";
+import { HammerIcon, ShoppingCartIcon, SquareMenu, User } from "lucide-react";
 import "../../styles/shopping-view/header.css";
+import { Link } from "react-router-dom";
+
+const navList = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/electronics",
+    name: "Electronics",
+  },
+  {
+    path: "/jewelery",
+    name: "Jewelery",
+  },
+  {
+    path: "/men's clothing",
+    name: "Men's clothing",
+  },
+  {
+    path: "/women's clothing",
+    name: "Women's clothing",
+  },
+];
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">MineKart</div>
+
+      <div className="nav-items">
+        <ul>
+          {navList.map((item) => (
+            <li key={item.name}>
+              <Link to={item.path}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <div className="left-content">
         <span className="user-btn">
           <User />
         </span>
         <span className="user-btn">
-         <ShoppingCartIcon/>
+          <ShoppingCartIcon />
         </span>
       </div>
+
+      <button className="hamburger-btn">
+        <SquareMenu size={40} />
+      </button>
     </nav>
   );
 };
